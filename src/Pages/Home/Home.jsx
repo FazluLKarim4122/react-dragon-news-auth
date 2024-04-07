d5165fd7e7a3372283ba9d2793f074eb127fa281
FazluLKarim4122 <md.fazlulkarim4122@gmail..com>
2024-04-07 21:20:56 +0600

    project setup with font completed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-poppins font-extrabold">this is home</h2>
+        </div>
+    );
+};
+
+export default Home;
